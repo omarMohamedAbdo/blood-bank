3b532680188522d66ea080fb1d764420060717ab
omarMohamedAbdo <omarknighto@gmail.com>
2020-06-19 08:07:39 +0200

    Editing Guest Welcome Home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,104 +1,202 @@
-<!DOCTYPE html>
+
+
+
+<!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+     <!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="icon" href="img/favicon.png" type="image/png">
+	<title>Blood Bank</title>
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="{{asset('css/css/bootstrap.css')}}">
+	<link rel="stylesheet" href="{{asset('css/vendors/linericon/style.css')}}">
+	<link rel="stylesheet" href="{{asset('css/css/font-awesome.min.css')}}">
+	<link rel="stylesheet" href="{{asset('css/vendors/owl-carousel/owl.carousel.min.css')}}">
+	<link rel="stylesheet" href="{{asset('css/vendors/lightbox/simpleLightbox.css')}}">
+	<link rel="stylesheet" href="{{asset('css/vendors/nice-select/css/nice-select.css')}}">
+	<link rel="stylesheet" href="{{asset('css/vendors/animate-css/animate.css')}}">
+    <link rel="stylesheet" href="{{asset('css/vendors/jquery-ui/jquery-ui.css')}}">
+    <link rel="stylesheet" href="{{asset('css/css/magnific-popup.css')}}">
+	<!-- main css -->
+	<link rel="stylesheet" href="{{asset('css/css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('css/css/responsive.css')}}">
+	<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+</head>
+<body>
+     <!--================Header Menu Area =================-->
+	<header class="header_area">
+		<div class="main_menu">
+			<nav class="navbar navbar-expand-lg navbar-light">
+				<div class="container">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<a class="navbar-brand logo_h" href="/">
+						<img src="{{asset('css/img/logo.png')}}" alt="">
+					</a>
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+					 aria-expanded="false" aria-label="Toggle navigation">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse offset" id="navbarSupportedContent">
+						<div class="row ml-0 w-100">
+							<div class="col-lg-12 pr-0">
+								<ul class="nav navbar-nav center_nav pull-right">
+                                    @if(Auth::check())
+                                    <li class="nav-item ">
+                                        <a class="nav-link" href="{{ url('/home') }}">Home</a>
+                                    </li>
+                                    @elseif(Auth::guard('hospital')->check())
+                                    <li class="nav-item ">
+                                        <a class="nav-link" href="{{ url('/hospital') }}">Hospital Home</a>
+                                        </li>
+                                    @else
+                                    <li class="nav-item ">
+                                        <a class="nav-link" href="{{ route('login') }}">Login</a>
+                                        </li>
+                                        <li class="nav-item ">
+                                        <a class="nav-link" href="{{ route('register') }}">Register</a>
+                                        </li>
+                                        <li class="nav-item ">
+                                        <a class="nav-link" href="{{ route('hospitalLogin') }}">Hospital Login</a>
+                                        </li>
+                                        <li class="nav-item">
+                                        <a class="nav-link" href="{{ route('hospitalRegister') }}">Hospital Register</a>
+                                        </li>
 
-        <title>Laravel</title>
+                                        <!-- @if (Route::has('register'))
+                                        @endif -->
+                                    @endif
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</nav>
+		</div>
+	</header>
+	<!--================Header Menu Area =================-->
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+       <!--================ Home Banner Area =================-->
+<section class="home_banner_area">
+		<div class="overlay"></div>
+		<div class="banner_inner d-flex align-items-center">
+			<div class="container">
+				<div class="banner_content row">
+					<div class="offset-lg-2 col-lg-8">
+						<img class="img-fluid" src="{{asset('css/img/banner/text-img.png')}}" alt="">
+						<p>If you are looking at blank cassettes on the web, you may be very confused at the difference in price You may see some
+							for as low as each.</p>
+						<!-- <a class="main_btn mr-10" href="#">donate now</a> -->
+						<a class="white_bg_btn" href="#">view activity</a>
+					</div>
+				</div>
+			</div>
+		</div>
+</section>
+	<!--================ End Home Banner Area =================-->
+      
+<!--================ Start Experience Area =================-->
+<section class="experience_donation section_gap">
+		<div class="container">
+			<div class="row align-items-center justify-content-center">
+				<div class="col-lg-12">
+					<h1>Experience How your Donation Can Reach</h1>
+					<p>he French Revolution constituted for the conscience of the dominant aristocratic class a fall from innocence, and upturning
+						of the natural chain of events that resounded.</p>
+					<a href="#" class="main_btn2 mr-10">make donation now</a>
+					<a href="#" class="main_btn2">Create Fundraising today</a>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--================ End Experience Area =================-->
+	<!--================ Start Footer Area  =================-->
+	<footer class="footer-area section_gap">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-5  col-md-6 col-sm-6">
+					<div class="single-footer-widget">
+						<h6 class="footer_title">About Me</h6>
+						<p>
+							Do you want to be even more successful? Learn to love learning and growth. The more effort you put into improving your skills,
+						</p>
+					</div>
+				</div>
+				<div class="col-lg-5 col-md-6 col-sm-6">
+					<div class="single-footer-widget">
+						<h6 class="footer_title">Newsletter</h6>
+						<p>Stay updated with our latest trends</p>
+						<div id="mc_embed_signup">
+							<form target="_blank" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01"
+							 method="get" class="subscribe_form relative">
+								<div class="input-group d-flex flex-row">
+									<input name="EMAIL" placeholder="Enter Email Address" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Email Address '"
+									 required="" type="email">
+									<button class="btn sub-btn">
+										<span class="lnr lnr-arrow-right"></span>
+									</button>
+								</div>
+								<div class="mt-10 info"></div>
+							</form>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-2 col-md-6 col-sm-6">
+					<div class="single-footer-widget f_social_wd">
+						<h6 class="footer_title">Follow Us</h6>
+						<p>Let us be social</p>
+						<div class="f_social">
+							<a href="#">
+								<i class="fa fa-facebook"></i>
+							</a>
+							<a href="#">
+								<i class="fa fa-twitter"></i>
+							</a>
+							<a href="#">
+								<i class="fa fa-dribbble"></i>
+							</a>
+							<a href="#">
+								<i class="fa fa-behance"></i>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if(Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @elseif(Auth::guard('hospital')->check())
-                         <a href="{{ url('/hospital') }}">Hospital Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                        <a href="{{ route('hospitalLogin') }}">Hospital Login</a>
-                        <a href="{{ route('hospitalRegister') }}">Hospital Register</a>
-
-                        <!-- @if (Route::has('register'))
-                        @endif -->
-                    @endif
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
+			<div class="row">
+				<div class="col-lg-12">
+					<p class="copyright"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+					</p>
+				</div>
+			</div>
+		</div>
+	</footer>
+    <!--================ End Footer Area  =================-->
+    
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="{{asset('js/js/jquery-3.2.1.min.js')}}"></script>
+<script src="{{asset('js/js/popper.js')}}"></script>
+<script src="{{asset('js/js/bootstrap.min.js')}}"></script>
+<!-- <script src="vendors/lightbox/simpleLightbox.min.js"></script> -->
+<script src="{{asset('css/vendors/nice-select/js/jquery.nice-select.min.js')}}"></script>
+<!-- <script src="vendors/isotope/imagesloaded.pkgd.min.js"></script> -->
+<script src="{{asset('css/vendors/isotope/isotope-min.js')}}"></script>
+<script src="{{asset('css/vendors/owl-carousel/owl.carousel.min.js')}}"></script>
+<script src="{{asset('js/js/jquery.ajaxchimp.min.js')}}"></script>
+<!-- <script src="vendors/counter-up/jquery.waypoints.min.js"></script> -->
+<!-- <script src="vendors/flipclock/timer.js"></script> -->
+<!-- <script src="vendors/counter-up/jquery.counterup.js"></script> -->
+<script src="{{asset('js/js/mail-script.js')}}"></script>
+<script src="{{asset('js/js/custom.js')}}"></script>
+    
+</body>
 </html>
+
