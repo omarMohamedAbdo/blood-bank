4eb5d6b62be89c95f6a58d63dfeed09b8665020a
omarMohamedAbdo <omarknighto@gmail.com>
2020-06-20 16:54:25 +0200

    Adding Statistics to user Home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
+use App\Donation;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,45 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        // return view('home');
+        $donations =  Donation::where('user_id', Auth::id() )->with('user', 'donorHospital')->get();
+        $i=0;
+        foreach($donations as $row) {
+            $Data[$i] = array
+            (
+              "value" => $row->donations_amount,
+              "name" => $row->request->name,
+            );
+            $i++;
+            // $Data['data'][] = (int) $row->count;
+          }
+        // $Data = array
+        //           (
+
+                    
+        //             "0" => array
+        //                             (
+        //                               "value" => 335,
+        //                               "name" => "Apple",
+        //                             ),
+        //             "1" => array
+        //                             (
+        //                               "value" => 310,
+        //                               "name" => "Orange",
+        //                             )
+        //                             ,
+        //             "2" => array
+        //                             (
+        //                               "value" => 234,
+        //                               "name" => "Grapes",
+        //                             )
+        //                             ,
+        //             "3" => array
+        //                             (
+        //                               "value" => 135,
+        //                               "name" => "Banana",
+        //                             )
+        //           );
+        return view('home',['Data' => $Data]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,58 @@
 @extends('layouts.donor')
 
+@section('css')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/echarts/4.1.0/echarts.min.js"></script>
+<script>
+$(function(){
+  'use strict'
+ 
+  /**************** PIE CHART ************/
+  var pieData = [{
+    name: 'My Donations',
+    type: 'pie',
+    radius: '80%',
+    center: ['50%', '57.5%'],
+    data: <?php echo json_encode($Data); ?>,
+    label: {
+      normal: {
+        fontFamily: 'Roboto, sans-serif',
+        fontSize: 11
+      }
+    },
+    labelLine: {
+      normal: {
+        show: false
+      }
+    },
+    markLine: {
+      lineStyle: {
+        normal: {
+          width: 1
+        }
+      }
+    }
+  }];
+  var pieOption = {
+    tooltip: {
+      trigger: 'item',
+      formatter: '{a} <br/>{b}: {c} ({d}%)',
+      textStyle: {
+        fontSize: 11,
+        fontFamily: 'Roboto, sans-serif'
+      }
+    },
+    legend: {},
+    series: pieData
+  };
+  var pie = document.getElementById('chartPie');
+  var pieChart = echarts.init(pie);
+  pieChart.setOption(pieOption);
+   /** making all charts responsive when resize **/
+});
+</script>
+
+@endsection
 @section('content')
 
      <!--================ Home Banner Area =================-->
@@ -20,5 +73,7 @@
 		</div>
 </section>
 	<!--================ End Home Banner Area =================-->
-  
+<section class="contact_area p_120"> 
+	<div id="chartPie" style="height: 550px;"></div>
+</section>
 @endsection
