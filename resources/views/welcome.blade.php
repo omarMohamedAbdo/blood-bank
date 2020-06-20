5dafaf6b84ed3e7413d08a99bf44cc8765fa8ad4
omarMohamedAbdo <omarknighto@gmail.com>
2020-06-20 15:45:45 +0200

    Adding Team to Guest Home Page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,6 +23,48 @@
 	<link rel="stylesheet" href="{{asset('css/css/style.css')}}">
     <link rel="stylesheet" href="{{asset('css/css/responsive.css')}}">
 	<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+
+	<style>
+			@import url(//fonts.googleapis.com/css?family=Montserrat:300,500);
+		.team4 {
+		font-family: "Montserrat", sans-serif;
+			color: #8d97ad;
+		font-weight: 300;
+		}
+
+		.team4 h1, .team4 h2, .team4 h3, .team4 h4, .team4 h5, .team4 h6 {
+		color: #3e4555;
+		}
+
+		.team4 .font-weight-medium {
+			font-weight: 500;
+		}
+
+		.team4 h5 {
+			line-height: 22px;
+			font-size: 18px;
+		}
+
+		.team4 .subtitle {
+			color: #8d97ad;
+			line-height: 24px;
+				font-size: 13px;
+		}
+
+		.team4 ul li a {
+		color: #8d97ad;
+		padding-right: 15px;
+		-webkit-transition: 0.1s ease-in;
+		-o-transition: 0.1s ease-in;
+		transition: 0.1s ease-in;
+		}
+
+		.team4 ul li a:hover {
+		-webkit-transform: translate3d(0px, -5px, 0px);
+		transform: translate3d(0px, -5px, 0px);
+			color: #316ce8;
+		}
+	</style>
 </head>
 <body>
      <!--================Header Menu Area =================-->
@@ -99,7 +141,87 @@
 		</div>
 </section>
 	<!--================ End Home Banner Area =================-->
-      
+
+<section class="">
+	<div class="py-5 team4">
+	<div class="container">
+		<div class="row justify-content-center mb-4">
+		<div class="col-md-7 text-center">
+			<h3 class="mb-3">Our Amazing Team</h3>
+			<h6 class="subtitle">You can relay on our amazing features list and also our customer services will be great experience for you without doubt and in no-time</h6>
+		</div>
+		</div>
+		<div class="row">
+		<!-- column  -->
+		<div class="col-lg-4 mb-4">
+			<!-- Row -->
+			<div class="row">
+			<div class="col-md-12">
+				<img src="{{asset('OmarFormal.jpg')}}" alt="wrapkit" class="img-fluid rounded-circle" />
+			</div>
+			<div class="col-md-12 text-center">
+				<div class="pt-2">
+				<h5 class="mt-4 font-weight-medium mb-0">Omar Abdo</h5>
+				<h6 class="subtitle mb-3">Software Engineer</h6>
+				<p>You can relay on our amazing features list and also our customer services will be great experience.</p>
+				<ul class="list-inline">
+				<li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="fa fa-linkedin fa-2x" aria-hidden="true"></i></a></li>
+					<li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="fa fa-github fa-2x" aria-hidden="true"></i></a></li>
+				</ul>
+				</div>
+			</div>
+			</div>
+			<!-- Row -->
+		</div>
+		<!-- column  -->
+		<!-- column  -->
+		<div class="col-lg-4 mb-4">
+			<!-- Row -->
+			<div class="row">
+			<div class="col-md-12">
+				<img src="{{asset('Dalia.jpeg')}}" style="width: 450px;" alt="wrapkit" class="img-fluid rounded-circle" />
+			</div>
+			<div class="col-md-12 text-center">
+				<div class="pt-2">
+				<h5 class="mt-4 font-weight-medium mb-0">Dalia Mohamed</h5>
+				<h6 class="subtitle mb-3">Software Engineer</h6>
+				<p>You can relay on our amazing features list and also our customer services will be great experience.</p>
+				<ul class="list-inline">
+				<li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="fa fa-linkedin fa-2x" aria-hidden="true"></i></a></li>
+					<li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="fa fa-github fa-2x" aria-hidden="true"></i></a></li>
+				</ul>
+				</div>
+			</div>
+			</div>
+			<!-- Row -->
+		</div>
+		<!-- column  -->
+		
+		<!-- column  -->
+		<div class="col-lg-4 mb-4">
+			<!-- Row -->
+			<div class="row">
+			<div class="col-md-12">
+				<img src="{{asset('Mahmoud.jpeg')}}" style="width: 450px;" alt="wrapkit" class="img-fluid rounded-circle" />
+			</div>
+			<div class="col-md-12 text-center">
+				<div class="pt-2">
+				<h5 class="mt-4 font-weight-medium mb-0">Mahmoud Assem</h5>
+				<h6 class="subtitle mb-3">Software Engineer</h6>
+				<p>You can relay on our amazing features list and also our customer services will be great experience.</p>
+				<ul class="list-inline">
+					<li class="list-inline-item"><a href="https://www.linkedin.com/in/mahmoud-assem-30b7a9153/" class="text-decoration-none d-block px-1"><i class="fa fa-linkedin fa-2x" aria-hidden="true"></i></a></li>
+					<li class="list-inline-item"><a href="#" class="text-decoration-none d-block px-1"><i class="fa fa-github fa-2x" aria-hidden="true"></i></a></li>
+				</ul>
+				</div>
+			</div>
+			</div>
+			<!-- Row -->
+		</div>
+		</div>
+	</div>
+	</div>
+</section>	
 <!--================ Start Experience Area =================-->
 <section class="experience_donation section_gap">
 		<div class="container">
