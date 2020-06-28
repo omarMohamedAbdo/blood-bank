cb8ed6c3b54ff0554c8cdfbe38340cc92f222e05
DaliaMatter <daliaamohamed243@gmail.com>
2020-06-28 14:25:37 +0200

    enchance in UI.

--- resources/views/donationsList.blade.php
+++ resources/views/donationsList.blade.php
@@ -1,4 +1,12 @@
 @extends('layouts.hospital')
+@section('css')
+<style>
+    .genric-btn{
+        padding: 0 10px;
+    }
+</style>
+
+@endsection
 
     @section('content')
     <section class="contact_area p_120">
@@ -18,63 +26,65 @@
             </div>
         @endif
             <div class="row justify-content-center">
-                <div class="col-md-8">
+                <div class="col-md-9">
                     <h3 class="mb-30 title_color">Donations</h3>
                     <div class="progress-table-wrap">
-                        <div class="progress-table">
-                            <div class="table-head">
-                                <div class="serial">ID</div>
-                                <div class="visit">Name</div>
-                                <div class="visit">Donar</div>
-                                <div class="visit">blood Type</div>
-                                <div class="visit">Amount</div>
-                                <div class="visit">Accept</div>
-                                <div class="visit">Reject</div>
-                            </div>
-                            @foreach( $allDonations as $donations)
-                                @foreach ($donations as $donation)
-                                    @if($donation->status == "pending")
-                                    <div class="table-row">
-                                        <div class="serial">{{ $donation->request_id }}</div>
-                                        <div class="visit"> {{$donation->request->name}} </div>
-                                        <div class="visit">
+                        <table class="table table-striped" style=" border: solid #ff8080;">
+                            <thead>
+                                <tr>
+                                    <th scope="col">ID</th>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Donar</th>
+                                    <th scope="col">Blood Type</th>
+                                    <th scope="col">Amount</th>
+                                    <th scope="col">Accept</th>
+                                    <th scope="col">Reject</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach( $allDonations as $donations)
+                                    @foreach ($donations as $donation)
+                                        @if($donation->status == "pending")
+                                        <tr>
+                                            <th scope="row">{{ $donation->request_id }}</th>
+                                            <td> {{$donation->request->name}} </td>
+                                            <td>
+                                                @if(@isset($donation->user_id))
+                                                    <a href="{{ url('/hospital/donors/'.$donation->user_id) }}" >{{ $donation->user->name }}</a>
+                                                @else
+                                                    <a>{{ $donation->donorHospital->name }}</a>
+                                                @endif
+                                            </td>
                                             @if(@isset($donation->user_id))
-                                                <a href="{{ url('/hospital/donors/'.$donation->user_id) }}" >{{ $donation->user->name }}</a>
+                                                <td> {{ $donation->user->blood_type}} </td>
                                             @else
-                                                <a>{{ $donation->donorHospital->name }}</a>
+                                            <td> {{$donation->blood_type}} </td>
                                             @endif
-                                        </div>
-                                        @if(@isset($donation->user_id))
-                                            <div class="visit">{{ $donation->user->blood_type}}</div>
-                                        @else
-                                        <div class="visit">{{ $donation->blood_type}}</div>
-                                        @endif
-                                        
-                                        <div class="visit">{{ $donation->donations_amount}}</div>
-                                        <div class="visit">
-                                            @if(isset($donation->user->last_test) || isset($donation->hospital_id))
-                                                <form action="{{route('donations.update',$donation)}}" method="POST" enctype="multipart/form-data">
-                                                @csrf
-                                                @method('PUT')
-                                                    {!! Form::button ('<a class="genric-btn success circle small">Accept</a>' ,['type' => 'submit' , 'class' => 'genric-btn ']) !!}
-                                                </form>
-                                                
+                                            
+                                            <td>{{ $donation->donations_amount}}</td>
+                                            <td>
+                                                @if(isset($donation->user->last_test) || isset($donation->hospital_id))
+                                                    <form action="{{route('donations.update',$donation)}}" method="POST" enctype="multipart/form-data">
+                                                    @csrf
+                                                    @method('PUT')
+                                                        {!! Form::button ('<a class="btn genric-btn success circle small">Accept</a>' ,['type' => 'submit' , 'class' => 'genric-btn ']) !!}
+                                                    </form>
                                                 @endif    
-                                            </div>
-                                        <div class="visit">
-                                            <form action="{{route('donations.destroy',$donation)}}" method="POST" >
-                                                {!! Form::button ('<a class="genric-btn danger circle small">Reject</a>' ,['type' => 'submit' , 'class' => 'genric-btn ']) !!}
-                                            @csrf
-                                            @method('DELETE')
-                                            </form>
-                                        </div>
-                                    </div>
-                                    @endif
+                                            </td>
+                                            <td>
+                                                <form action="{{route('donations.destroy',$donation)}}" method="POST" >
+                                                    {!! Form::button ('<a class="btn genric-btn danger circle small">Reject</a>' ,['type' => 'submit' , 'class' => 'genric-btn ']) !!}
+                                                @csrf
+                                                @method('DELETE')
+                                                </form>
+                                            </td>
+                                        </tr>
+                                        @endif
+                                    @endforeach
                                 @endforeach
-                            @endforeach
                             
-                        </div>
-                    </div>
+                            </tbody>
+                        </table>
                     {{--  --}}
                 </div>
             </div>
